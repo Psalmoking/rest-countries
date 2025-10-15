7bcade30849b2040a6b9a4178d787111457b7c75
Samuel Adeleke <sadeleke745@gmail.com>
2025-10-15 20:45:22 +0100

    fix: prevent crash on search focus

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -1,25 +1,3 @@
-// import { Search } from "lucide-react";
-// import { Input } from "../ui/input";
-
-// interface SearchBarProps {
-//   placeholder?: string;
-// }
-
-// const SearchBar = ({ placeholder }: SearchBarProps) => {
-//   return (
-//     <div className="relative w-full max-w-sm shadow-md rounded-md flex items-center">
-//       <Search className="absolute left-8 h-5 w-5 text-muted-foreground" />
-//       <Input
-//         type="search"
-//         placeholder={placeholder ?? "Search..."}
-//         className="pl-18 text-sm border-none h-15"
-//       />
-//     </div>
-//   );
-// };
-
-// export default SearchBar;
-
 import { Search } from "lucide-react";
 import { Input } from "../ui/input";
 
