f660f07043333bed0eb62755b30bee114fa65384
Samuel Adeleke <sadeleke745@gmail.com>
2025-10-15 14:52:03 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-red-500">Hello world!</h2>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
